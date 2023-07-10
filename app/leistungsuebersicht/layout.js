410e2978f530be7c9265a486d9d543f1468fc95b
AgencyIT <106689893+AgencyIT@users.noreply.github.com>
2023-07-10 13:43:01 +0200

    Update layout.js
    commented out the layout for leistungsübersicht

--- app/leistungsuebersicht/layout.js
+++ app/leistungsuebersicht/layout.js
@@ -1,9 +1,9 @@
-import '../globals.css'
+//import '../globals.css'
 // import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import Header from '../components/Header'
-import Content from '../components/Content'
-import Footer from '../components/Footer'
+//import Link from 'next/link'
+//import Header from '../components/Header'
+//import Content from '../components/Content'
+//import Footer from '../components/Footer'
 
 
 
@@ -24,23 +24,23 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="./css/all.min.css" />
-        <link rel="stylesheet" href="./css/min.css" />
-        <link rel="stylesheet" href="./css/menu.min.css" />
-        <link rel="stylesheet" href="./css/reset.css" />
-        <link rel="stylesheet" href="./css/styles.css" />
+        // <link rel="stylesheet" href="./css/all.min.css" />
+        // <link rel="stylesheet" href="./css/min.css" />
+        // <link rel="stylesheet" href="./css/menu.min.css" />
+        // <link rel="stylesheet" href="./css/reset.css" />
+        // <link rel="stylesheet" href="./css/styles.css" />
       </head>
       <body className="">
-        <Header />
-        <Content />
+        // <Header />
+        // <Content />
         {children}
-        <Footer />
+        // <Footer />
 
-        <script src="../script/min.js" />
+        // <script src="../script/min.js" />
 
-        <script src="../script/menu.js" />
+        // <script src="../script/menu.js" />
 
-        <script src="../script/custom.js" />
+        // <script src="../script/custom.js" />
       </body>
     </html>
   )
